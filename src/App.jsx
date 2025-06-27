976b8da322628096cdc5fe58074f01e325dafddd
Vellu <veli-matti.heikkila@helsinki.fi>
2025-06-27 15:23:09 +0300

    protected page directly under /admin url

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ const App = () => {
                     </AuthProvider>
                 }>
                     {/* Add all components that use /api calls here */}
-                    <Route path="protected" element={<Protected />} />
+                    <Route index element={<Protected />} />
                 </Route>
             </Route>
         ),
