1f04b7752c4c1a07ff618bc7e65da3d3e2e94d58
Riku Hänninen <riku.hanninen@helsinki.fi>
2025-07-10 13:02:07 +0300

    dsChange event listener handling for Chip

--- src/components/misc/ds/Chip.jsx
+++ src/components/misc/ds/Chip.jsx
@@ -1,13 +1,22 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const Chip = ({
   index = 0,
   text = '',
   icon,
+  onClick,
   ...rest
 }) => {
   const ref = useRef();
+
+  
+  useEffect(() => {
+    if (ref.current && onClick) {
+      ref.current.addEventListener('dsClick', onClick);
+    }
+  }, [ref.current, onClick]);
+  
 
   const dsProps = {
     index,
@@ -23,7 +32,8 @@ const Chip = ({
 Chip.propTypes = {
   index: PropTypes.number,
   text: PropTypes.string,
-  icon: PropTypes.string
+  icon: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Chip;
