eb8fb6509133fb8b5ecc6c0cd22891dcd70f0c62
Riku Hänninen <riku.hanninen@helsinki.fi>
2025-07-29 07:20:19 +0300

    Breadcrumb improvements

--- src/components/misc/breadcrumb/BreadCrumb.jsx
+++ src/components/misc/breadcrumb/BreadCrumb.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import './BreadCrumb.css'
+import React, { useEffect, useRef } from 'react';
 import Icon from '../ds/Icon';
+import './BreadCrumb.css';
 import Crumb from './Crumb';
-import { useTranslation } from 'react-i18next';
-
-const move = (() => {
-  let lastTouch = null;
-  return (event) => {
-    const currentX = event.touches[0].clientX;
-    if (!lastTouch) {
-      lastTouch = event.touches[0];
-      return;
-    }
-    const difference = currentX - lastTouch.clientX;
-    const breadCrumbContainer = event.currentTarget;
-    if (difference > 0) {
-      breadCrumbContainer.scrollBy({ left: -30, behavior: 'instant' });
-    } else if (difference < 0) {
-      breadCrumbContainer.scrollBy({ left: 30, behavior: 'instant' });
-    }
-    lastTouch = event.touches[0];
-  };
-})();
 
 const Separator = () => (
   <div aria-hidden className="bread-crumb-separator">
@@ -30,10 +10,7 @@ const Separator = () => (
   </div>
 );
 
-
 const BreadCrumb = ({ crumbs }) => {
-  const { t } = useTranslation();
-
   const firstRef = useRef(null);
   const lastRef = useRef(null);
   const navRef = useRef(null);
@@ -47,52 +24,29 @@ const BreadCrumb = ({ crumbs }) => {
 
 
   useEffect(() => {
-    if (firstRef.current && lastRef.current && containerRef.current) {
+    if (firstRef.current && lastRef.current) {
       const firstLeft = Math.abs(firstRef.current.getBoundingClientRect().left);
       const lastRight = Math.abs(lastRef.current.getBoundingClientRect().right);
       const minWidth = firstLeft + lastRight;
-      const mediaQuery = window.matchMedia(`(min-width: ${minWidth + 32}px)`);
+      const mediaQuery = window.matchMedia(`(min-width: ${minWidth}px)`);
 
-      const toggleButtons = (matches) => {
-        const btns = containerRef.current?.querySelectorAll('.bread-crumb-scroll-button');
-        if (matches && btns) {
-          btns.forEach(btn => btn.setAttribute('hidden', ''));
-        } else if (btns) {
-          btns.forEach(btn => btn.removeAttribute('hidden'));
+      const scrollToLastOne = (matches) => {
+        if (!matches) {
           lastRef.current.scrollIntoView(false);
         }
       };
 
       mediaQuery.addEventListener("change", (e) => {
-        toggleButtons(e.matches);
+        scrollToLastOne(e.matches);
       });
 
-      toggleButtons(mediaQuery.matches);
+      scrollToLastOne(mediaQuery.matches);
     }
-  }, [firstRef.current, lastRef.current, containerRef.current]);
-
-  const scroll = (event, direction) => {
-    switch (direction) {
-      case 'left':
-        navRef.current.scrollBy({ left: -50 });
-        break;
-      case 'right':
-        navRef.current.scrollBy({ left: 50 });
-        break;
-      default:
-        break;
-    }
-  };
+  }, [firstRef.current, lastRef.current]);
 
   return (
     <div ref={containerRef} className="bread-crumbs-container">
-      <div className="bread-crumb-scroll-button bread-crumb-scroll-button-left">
-        <button onClick={e => scroll(e, 'left')}>
-          <span className="screenreader-only">{t('bread_crumb_scroll_left')}</span>
-          <Icon name="chevron_backward" />
-        </button>
-      </div>
-      <nav ref={navRef} className="bread-crumbs" onTouchMove={move}>
+      <nav ref={navRef} className="bread-crumbs">
         {crumbs.map((crumb, i) => {
           let refProp = {};
           if (i === 0) {
@@ -107,12 +61,6 @@ const BreadCrumb = ({ crumbs }) => {
           </React.Fragment>;
         })}
       </nav>
-      <div className="bread-crumb-scroll-button bread-crumb-scroll-button-right">
-        <button onClick={e => scroll(e, 'right')}>
-          <span className="screenreader-only">{t('bread_crumb_scroll_right')}</span>
-          <Icon name="chevron_forward" />
-        </button>
-      </div>
     </div>
   );
 };
