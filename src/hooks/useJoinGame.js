db4f1b86e1fcff0756d5604202178405c11705b5
anttilan <antti.lankinen@helsinki.fi>
2025-09-02 11:36:13 +0300

    useJoinGame lisätty  `GAME_PLAYERS_${game.game_id}`

--- src/hooks/useJoinGame.js
+++ src/hooks/useJoinGame.js
@@ -3,7 +3,7 @@ import { usePOST } from "./useHttp";
 const useJoinGame = (game) => {
   const post = usePOST({
     path: `/public/games/join`,
-    invalidates: [`GAME_${game.game_id}`, `GAME_${game.game_code}`, `GAMES`]
+    invalidates: [`GAME_${game.game_id}`, `GAME_${game.game_code}`, `GAMES`, `GAME_PLAYERS_${game.game_id}`]
   });
 
   return post;
