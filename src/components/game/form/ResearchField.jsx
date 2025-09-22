86f6ecfadbee07cb6adec6955b56a0441b3317d3
jooaho <jooaho@helsinki.fi>
2025-09-22 14:14:33 +0300

    dsProps

--- src/components/game/form/ResearchField.jsx
+++ src/components/game/form/ResearchField.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './ResearchField.css';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
-import RadioButton from "../../misc/ds/RadioButton.jsx";
 import CheckBox from "../../misc/ds/CheckBox.jsx";
 
 const ResearchField = ({ value, onChange, disabled, validation }) => {
