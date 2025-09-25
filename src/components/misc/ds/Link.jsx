74a7cd5617aa214414e3e976cc56724bb1d395a6
jooaho <jooaho@helsinki.fi>
2025-09-25 10:26:30 +0300

    IM-102 use notification for start game validation messages

--- src/components/misc/ds/Link.jsx
+++ src/components/misc/ds/Link.jsx
@@ -1,97 +1,99 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
-import ErrorText from './../ds/ErrorText.jsx';
+import { useNotification } from '../../notification/NotificationContext.js';
 
 const Link = ({
-  label = '',
-  href = '',
-  'aria-label': ariaLabel = label,
-  colour = 'blue',
-  icon,
-  iconPosition = 'start',
-  weight = 'regular',
-  target = '_self',
-  size = 'medium',
-  variant = '',
-  internal = false,
-  validation,
-  ...rest
-}) => {
-  const navigate = useNavigate();
+                  label = '',
+                  href = '',
+                  'aria-label': ariaLabel = label,
+                  colour = 'blue',
+                  icon,
+                  iconPosition = 'start',
+                  weight = 'regular',
+                  target = '_self',
+                  size = 'medium',
+                  variant = '',
+                  internal = false,
+                  validation,
+                  ...rest
+              }) => {
+    const navigate = useNavigate();
+    const { setNotification } = useNotification();
 
-  const onClick = (event) => {
-    if (validation && !validation.isValid) {
-      event.preventDefault();
-      return;
-    }
-    if (internal) {
-      event.preventDefault();
-      navigate(href);
-    }
-  };
+    const onClick = (event) => {
+        if (validation && !validation.isValid) {
+            event.preventDefault();
+            setNotification(validation.message, 'error', true);
+            return;
+        }
+        if (internal) {
+            event.preventDefault();
+            navigate(href);
+        }
+    };
 
-  const dsProps = {
-    dsText: label,
-    dsHref: href,
-    dsAriaLabel: ariaLabel,
-    dsColour: colour,
-    dsIcon: icon,
-    dsIconPosition: iconPosition,
-    dsWeight: weight,
-    dsTarget: target,
-    dsSize: size,
-    dsVariant: variant,
-    ...rest
-  };
+    const dsProps = {
+        dsText: label,
+        dsHref: href,
+        dsAriaLabel: ariaLabel,
+        dsColour: colour,
+        dsIcon: icon,
+        dsIconPosition: iconPosition,
+        dsWeight: weight,
+        dsTarget: target,
+        dsSize: size,
+        dsVariant: variant,
+        ...rest
+    };
 
-  return (
-      <div className="ds-link-container">
-        <ds-link {...dsProps} onClick={onClick} />
-        <div className="ds-link-error">
-        {validation && !validation.isValid && (
-            <ErrorText text={validation.message} />
-        )}
+    return (
+        <div className="ds-link-container">
+            <ds-link {...dsProps} onClick={onClick} />
         </div>
-      </div>
-  );
+    );
 };
 
 Link.propTypes = {
-  label: PropTypes.string,
-  href: PropTypes.string,
-  'aria-label': PropTypes.string,
-  colour: PropTypes.oneOf([
-    'blue',
-    'black',
-    'white'
-  ]),
-  size: PropTypes.oneOf([
-    '2xLarge',
-    'large',
-    'medium',
-    'small',
-    'xLarge',
-    'xSmall'
-  ]),
-  icon: PropTypes.string,
-  iconPosition: PropTypes.string,
-  weight: PropTypes.oneOf([
-    'bold',
-    'regular',
-    'semibold'
-  ]),
-  target: PropTypes.oneOf([
-    '_blank',
-    '_parent',
-    '_self',
-    '_top',
-    '_unfencedTop'
-  ]),
-  variant: PropTypes.oneOf([
-    'inline',
-    'standalone'
-  ])
+    label: PropTypes.string,
+    href: PropTypes.string,
+    'aria-label': PropTypes.string,
+    colour: PropTypes.oneOf([
+        'blue',
+        'black',
+        'white'
+    ]),
+    size: PropTypes.oneOf([
+        '2xLarge',
+        'large',
+        'medium',
+        'small',
+        'xLarge',
+        'xSmall'
+    ]),
+    icon: PropTypes.string,
+    iconPosition: PropTypes.string,
+    weight: PropTypes.oneOf([
+        'bold',
+        'regular',
+        'semibold'
+    ]),
+    target: PropTypes.oneOf([
+        '_blank',
+        '_parent',
+        '_self',
+        '_top',
+        '_unfencedTop'
+    ]),
+    variant: PropTypes.oneOf([
+        'inline',
+        'standalone'
+    ]),
+    internal: PropTypes.bool,
+    validation: PropTypes.shape({
+        isValid: PropTypes.bool,
+        message: PropTypes.string
+    })
 };
 
 export default Link;
