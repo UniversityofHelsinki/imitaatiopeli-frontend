2983f160004e863260932de254537be0dc48ae7e
Vellu <veli-matti.heikkila@helsinki.fi>
2025-11-11 14:21:22 +0200

    accessibility fixes

--- src/components/misc/ds/Accordion.jsx
+++ src/components/misc/ds/Accordion.jsx
@@ -7,63 +7,63 @@ import { DsAccordion } from '@uh-design-system/component-library-react';
 
 
 const Accordion = ({
-  header = '',
-  content = '',
-  variant = 'default',
-  headingLevel = 2,
-  id,
-  openByDefault = false,
-  hideTopBorder = false,
-  hideBottomBorder = false,
-  showCloseButton = false,
-  alignBorders = false,
-  closeButtonLabel,
-  ...rest
-}) => {
-  const { t } = useTranslation();
-  const defaultId = useId();
+                       header = '',
+                       content = '',
+                       variant = 'default',
+                       headingLevel = 2,
+                       id,
+                       openByDefault = false,
+                       hideTopBorder = false,
+                       hideBottomBorder = false,
+                       showCloseButton = false,
+                       alignBorders = false,
+                       closeButtonLabel,
+                       ...rest
+                   }) => {
+    const { t } = useTranslation();
+    const defaultId = useId();
 
-  const dsProps = {
-    Items: 1,
-    dsVariant: variant,
-    dsUseCloseButton: showCloseButton,
-    dsBorderAligned: alignBorders,
-    dsOpenByDefault: openByDefault,
-    dsHeadingLevel: headingLevel,
-    dsAccordionId: id || defaultId,
-    dsHideTopBorder: hideTopBorder,
-    dsHideBottomBorder: hideBottomBorder,
-    dsCloseButtonLabel: closeButtonLabel || t('accordion_close'),
-    ...rest
-  };
+    const dsProps = {
+        Items: 1,
+        dsVariant: variant,
+        dsUseCloseButton: showCloseButton,
+        dsBorderAligned: alignBorders,
+        dsOpenByDefault: openByDefault,
+        dsHeadingLevel: headingLevel,
+        dsId: id || defaultId,
+        dsHideTopBorder: hideTopBorder,
+        dsHideBottomBorder: hideBottomBorder,
+        dsCloseButtonLabel: closeButtonLabel || t('accordion_close'),
+        ...rest,
+    };
 
-  return (<DsAccordion {...dsProps}>
+    return (<DsAccordion {...dsProps}>
     <span
-      role="heading"
-      aria-level={headingLevel}
-      className="accordion-header"
-      slot="header">
+        role="heading"
+        aria-level={headingLevel}
+        className="accordion-header"
+        slot="header">
       {header}
     </span>
-    <div slot="content">{content}</div>
-  </DsAccordion>);
+        <div slot="content">{content}</div>
+    </DsAccordion>);
 };
 
 Accordion.propTypes = {
-  header: PropTypes.node,
-  content: PropTypes.node,
-  variant: PropTypes.oneOf([
-    'compact',
-    'default'
-  ]),
-  headingLevel: PropTypes.number,
-  id: PropTypes.string,
-  openByDefault: PropTypes.bool,
-  alignBorders: PropTypes.bool,
-  hideTopBorder: PropTypes.bool,
-  hideBottomBorder: PropTypes.bool,
-  showCloseButton: PropTypes.bool,
-  closeButtonLabel: PropTypes.string,
+    header: PropTypes.node,
+    content: PropTypes.node,
+    variant: PropTypes.oneOf([
+        'compact',
+        'default',
+    ]),
+    headingLevel: PropTypes.number,
+    id: PropTypes.string,
+    openByDefault: PropTypes.bool,
+    alignBorders: PropTypes.bool,
+    hideTopBorder: PropTypes.bool,
+    hideBottomBorder: PropTypes.bool,
+    showCloseButton: PropTypes.bool,
+    closeButtonLabel: PropTypes.string,
 };
 
 export default Accordion;
